7675ad61b13736cb6bce65a5f54e5b3e39b6ef0a
leonardo329cf <leonardo329cf@gmail.com>
2021-08-13 20:41:34 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -103,9 +103,10 @@ public class Product implements Serializable {
 		this.date = date;
 	}
 
-	public void setCategories(Set<Category> categories) {
-		this.categories = categories;
+	public Set<Category> getCategories() {
+		return categories;
 	}
+	
 
 	@Override
 	public int hashCode() {
